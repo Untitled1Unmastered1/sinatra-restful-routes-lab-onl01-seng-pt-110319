2e4a6d8a727913d72da3f99d6429f13f4d10c564
GABRIEL PEREZ <gabriel1994.gp@gmail.com>
2020-03-17 20:40:23 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,31 @@
+require_relative '../../config/environment'
+require 'pry'
 class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
   end
 
-  # code actions here!
+  get '/recipes/new' do 
+    
+    erb :new 
+  end 
+  
+  post '/recipes' do 
+    @recipe = Recipe.create(:name => params[:recipe_name], :ingredients => params[:recipe_ingredients], :cook_time => params[:recipe_cook_time])
+  redirect to "/recipes/#{@recipe.id}"
+  end 
+  
+  get '/recipes' do 
+    @recipes = Recipe.all 
+    
+    erb :index
+  end 
+  
+  get '/recipes/:id' do 
+    @recipe = Recipe.find_by_id(params[:id])
+    
+    erb :show 
+  end 
 
 end
